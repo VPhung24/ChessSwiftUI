70eeb921ab7dc21aa3231164220b286b38f79e08
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-04-26 17:21:31 -0400

    init

--- ChessSwiftUI/ChessBoardView.swift
+++ ChessSwiftUI/ChessBoardView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ChessBoardView: View {
+    @State private var chessBoard = ChessBoard()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ForEach(0..<8, id: \.self) { row in
+                HStack {
+                    ForEach(0..<8, id: \.self) { col in
+                        ZStack {
+                            Rectangle()
+                                .fill((row + col) % 2 == 0 ? Color.white : Color.gray)
+                            if let piece = chessBoard.board[row][col] {
+                                ChessPieceView(piece: piece)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-struct ChessBoardView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ChessPieceView: View {
+    let piece: ChessPiece
+
+    var body: some View {
         ChessBoardView()
     }
 }
--- ChessSwiftUI/ChessModel.swift
+++ ChessSwiftUI/ChessModel.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+struct ChessPiece {
+    enum PieceType {
+        case king, queen, rook, bishop, knight, pawn
+    }
+
+    enum PieceColor {
+        case white, black
+    }
+
+    let type: PieceType
+    let color: PieceColor
+}
+
+struct ChessBoard {
+    var board: [[ChessPiece?]]
+
+    init() {
+        board = Array(repeating: Array(repeating: nil, count: 8), count: 8)
+        // Initialize the chessboard with the starting positions of the pieces.
+    }
+}
+
+extension ChessBoard {
+    func isValidMove(from: (Int, Int), to: (Int, Int)) -> Bool {
+        // Implement logic to determine if a move is valid.
+        // This may include checking if the path is clear, if the move is within the bounds of the board,
+        // if the destination is empty or contains an enemy piece, and if the move conforms to the rules
+        // for the specific type of piece.
+        return true
+    }
+
+    mutating func movePiece(from: (Int, Int), to: (Int, Int)) {
+        guard isValidMove(from: from, to: to) else { return }
+        // Update the board by moving the piece to the new position and setting the old position to nil.
+        board[to.0][to.1] = board[from.0][from.1]
+        board[from.0][from.1] = nil
+    }
+
+    func isKingInCheck(color: ChessPiece.PieceColor) -> Bool {
+        // Implement logic to determine if the king is in check.
+        // This may include checking if any enemy pieces have a valid move to the king's position.
+        
+        return true
+    }
+
+    func getPossibleMoves(for piece: ChessPiece, at position: (Int, Int)) -> [(Int, Int)] {
+        // Implement logic to determine a list of possible moves for a piece.
+        // This should include all legal moves for the specific type of piece.
+        return [(0, 0)];
+    }
+
+    func isCheckmate(color: ChessPiece.PieceColor) -> Bool {
+        // Implement logic to determine if the game is in checkmate.
+        // This may include checking if the king is in check and if there are no legal moves available
+        // for any of the player's pieces.
+        return true
+    }
+}
